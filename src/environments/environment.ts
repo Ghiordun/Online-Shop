489d6327fe244f7c1447418e5a60e7ed50096a15
Andrei Ghiordun <andreig@imprezzio.global>
2021-02-01 19:16:34 +0200

    Added  navbar.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCfpnja445AU2K1ygrFFtSSB_pmBoq_yts",
+    authDomain: "woodsy-7d0a7.firebaseapp.com",
+    projectId: "woodsy-7d0a7",
+    storageBucket: "woodsy-7d0a7.appspot.com",
+    messagingSenderId: "197365534681",
+    appId: "1:197365534681:web:7a5128a0dd273a52d493c8",
+    measurementId: "G-T6RNRW4333"
+  }
 };
 
 /*
